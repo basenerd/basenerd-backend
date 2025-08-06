998773a3355aba1aa18015b35cf405aa4352d282
Nick Labella <nicklabella6@gmail.com>
2025-08-05 23:31:52 -0700

    Add new standings page

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -6,5 +8,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/standings')
+def standings():
+    resp = requests.get("https://statsapi.mlb.com/api/v1/standings?season=2025")
+    records = resp.json()["records"]
+    return render_template('standings.html', stats=records)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
