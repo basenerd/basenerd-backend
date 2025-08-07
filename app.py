3fc0af068c76172dd1c18b15617b511113a08543
Nick Labella <nicklabella6@gmail.com>
2025-08-07 00:23:48 -0700

    Fix standings page rendering

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 import requests
 
@@ -10,11 +9,12 @@ def home():
 
 @app.route('/standings')
 def standings():
-    resp = requests.get("https://statsapi.mlb.com/api/v1/standings?season=2025")
-    records = resp.json()["records"]
-    nl_divisions = [group for group in records if group["league"]["name"] == "National League"]
-    al_divisions = [group for group in records if group["league"]["name"] == "American League"]
-    return render_template('standings.html', nl_divisions=nl_divisions, al_divisions=al_divisions)
+    try:
+        resp = requests.get("https://statsapi.mlb.com/api/v1/standings?leagueId=103,104&season=2025&standingsTypes=regularSeason")
+        data = resp.json()
+        return render_template('standings.html', stats=data["records"])
+    except Exception as e:
+        return f"Error: {e}"
 
 if __name__ == '__main__':
     app.run(debug=True)
