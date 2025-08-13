11aab51e4b44861ade2004122edacef4a8a91b89
basenerd <nicklabella6@gmail.com>
2025-08-12 20:28:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request 
 import requests, logging
 from datetime import datetime, timezone, date
 from typing import List, Optional, Tuple
